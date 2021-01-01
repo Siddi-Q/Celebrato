ca13980c41c1adbc3cef9cf8ef20ab84e4d96a48
Saddique Shafi <saddiqueashafi@gmail.com>
2020-12-31 23:46:35 -0500

    Added react routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,27 @@
 import React from 'react';
 
+import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
+
+import Login from './components/login.component';
+import Signup from './components/signup.component';
+
 function App() {
   return (
-    <h1>App</h1>
+    <Router>
+      <nav>
+        <Link to="/login">Login</Link><br />
+        <Link to="/signup">Signup</Link>
+      </nav>
+
+      <Switch>
+        <Route exact path="/login">
+          <Login /> 
+        </Route>
+        <Route exact path="/signup">
+          <Signup />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
