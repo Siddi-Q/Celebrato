935dff500b0cf3d3ea51aec024790ae218175da4
Saddique Shafi <saddiqueashafi@gmail.com>
2021-01-03 17:22:52 -0500

    Cleaned up App.js; Updated routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,27 +7,27 @@ import Signup from './components/signup.component';
 import Home from './components/home.component';
 
 function App() {
-  return (
-    <Router>
-      <nav>
-        <Link to="/login">Login</Link><br />
-        <Link to="/signup">Signup</Link><br />
-        <Link to="/home">Home</Link>
-      </nav>
+    return (
+        <Router>
+            <nav>
+                <Link to="/login">Login</Link><br />
+                <Link to="/signup">Signup</Link><br />
+                <Link to="/home">Home</Link>
+            </nav>
 
-      <Switch>
-        <Route exact path="/login">
-          <Login /> 
-        </Route>
-        <Route exact path="/signup">
-          <Signup />
-        </Route>
-        <Route>
-          <Home />
-        </Route>
-      </Switch>
-    </Router>
-  );
+            <Switch>
+                <Route exact path={["/", "/login"]}>
+                    <Login /> 
+                </Route>
+                <Route exact path="/signup">
+                    <Signup />
+                </Route>
+                <Route exact path="/home">
+                    <Home />
+                </Route>
+            </Switch>
+        </Router>
+    );
 }
 
 export default App;
