108a12288787689718f653711795d290b507dd03
Saddique Shafi <saddiqueashafi@gmail.com>
2021-01-03 14:54:48 -0500

    Added MUI to signin component

--- frontend/src/components/signup.component.js
+++ frontend/src/components/signup.component.js
@@ -1,6 +1,44 @@
 import React, { useState } from 'react';
+import { Link as RouterLink } from 'react-router-dom';
 
-export default function Signup() {
+import Box from '@material-ui/core/Box';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Grid from '@material-ui/core/Grid';
+import Link from '@material-ui/core/Link';
+import Paper from '@material-ui/core/Paper';
+import TextField from '@material-ui/core/TextField';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+
+function Copyright() {
+    return (
+        <Typography variant="body2" color="textSecondary" align="center">
+            {"Celebrato ©"} {new Date().getFullYear()}
+        </Typography>
+    );
+}
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        height: '100vh',
+    },
+    paper: {
+        margin: theme.spacing(8, 4),
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    form: {
+        width: '100%',
+        marginTop: theme.spacing(1),
+    },
+    submit: {
+        margin: theme.spacing(3, 0, 2),
+    },
+}));
+
+export default function Login() {
     const [firstName, setFirstName] = useState('');
     const [lastName, setLastName] = useState('');
     const [email, setEmail] = useState('');
@@ -26,14 +64,57 @@ export default function Signup() {
         event.preventDefault();
         console.log("Submitted!");
     }
-
+    
+    const classes = useStyles();
+    
     return (
-        <form onSubmit={handleSubmit}>
-            <input onChange={handleFirstNameChange} placeholder="First Name" required type="" value={firstName} /><br />
-            <input onChange={handleLastNameChange} placeholder="Last Name" required type="" value={lastName} /><br />
-            <input onChange={handleEmailChange} placeholder="Email" required type="email" value={email} /><br />
-            <input onChange={handlePasswordChange} placeholder="Password" required type="password" value={password} /><br />
-            <input type="submit" value="Sign up" />
-        </form>
+        <Grid container component="div" className={classes.root}>
+            <CssBaseline />
+            <Grid item xs={12} sm={4} md={7}>
+                <Typography>Celebrato</Typography>
+            </Grid>
+            <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+                <div className={classes.paper}>
+                    <Typography component="h2" variant="h5">Sign up</Typography>
+                    <form className={classes.form} onSubmit={handleSubmit}>
+                        <TextField
+                            autoFocus autoComplete="given-name"
+                            fullWidth margin="normal" variant="outlined"
+                            label="First Name" type="text" required
+                            onChange={handleFirstNameChange} value={firstName}/>
+                        <TextField
+                            autoComplete="family-name"
+                            fullWidth margin="normal" variant="outlined"
+                            label="Last Name" type="text" required
+                            onChange={handleLastNameChange} value={lastName}/>
+                        <TextField
+                            autoComplete="email"
+                            fullWidth margin="normal" variant="outlined"
+                            label="Email" type="email" required
+                            onChange={handleEmailChange} value={email}/>
+                        <TextField
+                            autoComplete="current-password"
+                            fullWidth margin="normal" variant="outlined"
+                            label="Password" type="password" required
+                            onChange={handlePasswordChange} value={password}/>
+                        <Button
+                            className={classes.submit} color="primary" fullWidth variant="contained"
+                            type="submit">
+                            Sign up
+                        </Button>
+
+                        <Grid container>
+                            <Grid item>
+                                <Link variant="body2" component={RouterLink} to="/login">Have an account? Log in</Link>
+                            </Grid>
+                        </Grid>
+
+                        <Box mt={5}>
+                            <Copyright />
+                        </Box>
+                    </form>
+                </div>
+            </Grid>
+        </Grid>
     );
 }
