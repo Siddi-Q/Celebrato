6c407dc41d9bf722c8f49e423c857a64b81b850b
Saddique Shafi <saddiqueashafi@gmail.com>
2021-01-03 17:22:12 -0500

    Cleaned up login.component.js

--- frontend/src/components/login.component.js
+++ frontend/src/components/login.component.js
@@ -1,33 +1,3 @@
-// import React, { useState } from 'react';
-
-// export default function Login() {
-//     const [email, setEmail] = useState('');
-//     const [password, setPassword] = useState('');
-
-//     const handleEmailChange = (event) => {
-//         setEmail(event.target.value);
-//     }
-
-//     const handlePasswordChange = (event) => {
-//         setPassword(event.target.value);
-//     }
-
-//     const onSubmit = (event) => {
-//         event.preventDefault();
-//         console.log(email, password);
-//         setEmail('');
-//         setPassword('');
-//     }
-
-//     return (
-//         <form onSubmit={onSubmit}>
-//             <input onChange={handleEmailChange} placeholder="Email" required type="email" value={email}/><br />
-//             <input onChange={handlePasswordChange} placeholder="Password" required type="password" value={password} /><br />
-//             <input type="submit" value="Log in" />
-//         </form>
-//     );
-// }
-
 import React, { useState } from 'react';
 import { Link as RouterLink } from 'react-router-dom';
 
@@ -42,30 +12,30 @@ import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 
 function Copyright() {
-  return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      {"Celebrato ©"} {new Date().getFullYear()}
-    </Typography>
-  );
+    return (
+        <Typography variant="body2" color="textSecondary" align="center">
+            {"Celebrato ©"} {new Date().getFullYear()}
+        </Typography>
+    );
 }
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    height: '100vh',
-  },
-  paper: {
-    margin: theme.spacing(8, 4),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-  },
-  form: {
-    width: '100%',
-    marginTop: theme.spacing(1),
-  },
-  submit: {
-    margin: theme.spacing(3, 0, 2),
-  },
+    root: {
+        height: '100vh',
+    },
+    paper: {
+        margin: theme.spacing(8, 4),
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    form: {
+        width: '100%',
+        marginTop: theme.spacing(1),
+    },
+    submit: {
+        margin: theme.spacing(3, 0, 2),
+    },
 }));
 
 export default function Login() {
@@ -86,49 +56,47 @@ export default function Login() {
         setEmail('');
         setPassword('');
     }
-  const classes = useStyles();
 
-  return (
-    <Grid container component="main" className={classes.root}>
-      <CssBaseline />
-      <Grid item xs={12} sm={4} md={7}>
-          <Typography>Celebrato</Typography>
-        </Grid>
-      <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
-        <div className={classes.paper}>
-          <Typography component="h2" variant="h5">
-            Log in
-          </Typography>
-          <form className={classes.form} onSubmit={onSubmit}>
-            <TextField
-                autoFocus autoComplete="email"
-                fullWidth margin="normal" variant="outlined"
-                label="Email" type="email" required
-                onChange={handleEmailChange} value={email}/>
-            <TextField
-                autoComplete="current-password"
-                fullWidth margin="normal" variant="outlined"
-                label="Password" type="password" required
-                onChange={handlePasswordChange} value={password}/>
-            <Button
-                className={classes.submit} color="primary" fullWidth variant="contained"
-                type="submit">
-                Log in
-            </Button>
+    const classes = useStyles();
 
-            <Grid container>
-              <Grid item>
-                <Link variant="body2" component={RouterLink} to="/signup">Don't have an account? Sign Up</Link>
-              </Grid>
+    return (
+        <Grid container component="main" className={classes.root}>
+            <CssBaseline />
+            <Grid item xs={12} sm={4} md={7}>
+                <Typography>Celebrato</Typography>
             </Grid>
+            <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+                <div className={classes.paper}>
+                    <Typography component="h2" variant="h5">Log in</Typography>
+                    <form className={classes.form} onSubmit={onSubmit}>
+                        <TextField
+                            autoFocus autoComplete="email"
+                            fullWidth margin="normal" variant="outlined"
+                            label="Email" type="email" required
+                            onChange={handleEmailChange} value={email}/>
+                        <TextField
+                            autoComplete="current-password"
+                            fullWidth margin="normal" variant="outlined"
+                            label="Password" type="password" required
+                            onChange={handlePasswordChange} value={password}/>
+                        <Button
+                            className={classes.submit} color="primary" fullWidth variant="contained"
+                            type="submit">
+                            Log in
+                        </Button>
 
-            <Box mt={5}>
-              <Copyright />
-            </Box>
-            
-          </form>
-        </div>
-      </Grid>
-    </Grid>
-  );
+                        <Grid container>
+                            <Grid item>
+                                <Link variant="body2" component={RouterLink} to="/signup">Don't have an account? Sign Up</Link>
+                            </Grid>
+                        </Grid>
+
+                        <Box mt={5}>
+                            <Copyright />
+                        </Box>
+                    </form>
+                </div>
+            </Grid>
+        </Grid>
+    );
 }
