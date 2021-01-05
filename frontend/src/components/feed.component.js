412cf3cbc8aca33c30110f54e6f2694388aa1b98
Saddique Shafi <saddiqueashafi@gmail.com>
2021-01-05 15:21:22 -0500

    Updated to include the post's id

--- frontend/src/components/feed.component.js
+++ frontend/src/components/feed.component.js
@@ -6,8 +6,8 @@ import Post from './post.component';
 export default function Feed() {
     const posts = useSelector(state => state.posts);
 
-    const renderedPosts = posts.map((post, index) => (
-        <Post key={index} content={post.content} />
+    const renderedPosts = posts.map(post => (
+        <Post key={post.id} content={post.content} />
     ));
 
     return (
