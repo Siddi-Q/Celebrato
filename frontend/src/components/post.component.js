731868b88bcee61422ddf3ff1e1928c4648b74a8
Saddique Shafi <saddiqueashafi@gmail.com>
2021-01-05 19:18:30 -0500

    Refactored code; Moved code to editPostForm

--- frontend/src/components/post.component.js
+++ frontend/src/components/post.component.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
 
-import { postUpdated } from '../slices/postsSlice';
+import EditPostForm from './editPostForm.component';
 
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
@@ -9,68 +8,33 @@ import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardHeader from '@material-ui/core/CardHeader';
-import Dialog from '@material-ui/core/Dialog';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogTitle from '@material-ui/core/DialogTitle';
 import IconButton from '@material-ui/core/IconButton';
-import TextField from '@material-ui/core/TextField';
 import Typography from '@material-ui/core/Typography';
 
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 
 export default function Post(props) {
-    const [post, setPost] = useState(props.content);
     const [open, setOpen] = useState(false);
-    
-    const dispatch = useDispatch();
-
-    const handlePostChange = (event) => {
-        setPost(event.target.value);
-    }
-
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        console.log("Submitted Post!", props, post);
-        dispatch(postUpdated({id: props.id, content: post}));
-        handleClose();
-    }
 
     const handleClick = () => setOpen(true);
-    const handleClose = () => setOpen(false);
 
     return (
         <>
-        <Card>
-            <CardHeader
-                avatar={<Avatar>A</Avatar>}
-                action={<IconButton><MoreHorizIcon /></IconButton>}
-                title="FirstName LastName"
-                subheader="Date"
-            />
-            <CardContent>
-                <Typography>{props.content}</Typography>
-            </CardContent>
-            <CardActions>
-                <Button onClick={handleClick}>Edit Post</Button>
-            </CardActions>
-        </Card>
-        <Dialog open={open} onClose={handleClose}>
-            <DialogTitle>Edit Post</DialogTitle>
-            <DialogContent>
-                <form onSubmit={handleSubmit}>
-                    <TextField 
-                        fullWidth margin="normal" variant="outlined"
-                        placeholder="What are you celebrating?"
-                        multiline required type="text"
-                        onChange={handlePostChange} value={post}/>
-                    <Button
-                        color="primary" fullWidth variant="contained"
-                        type="submit">
-                        Save
-                    </Button>
-                </form>
-            </DialogContent>
-        </Dialog>
+            <Card>
+                <CardHeader
+                    avatar={<Avatar>A</Avatar>}
+                    action={<IconButton><MoreHorizIcon /></IconButton>}
+                    title="FirstName LastName"
+                    subheader="Date"
+                />
+                <CardContent>
+                    <Typography>{props.content}</Typography>
+                </CardContent>
+                <CardActions>
+                    <Button onClick={handleClick}>Edit Post</Button>
+                </CardActions>
+            </Card>
+            <EditPostForm isOpen={open} setOpen={setOpen} id={props.id} content={props.content} />
         </>
     );
 }
