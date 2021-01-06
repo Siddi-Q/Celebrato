97a5ba099ad19d2286717b8fc8b142b93a347e16
Saddique Shafi <saddiqueashafi@gmail.com>
2021-01-05 19:17:25 -0500

    Created editPostForm component

---
+++ frontend/src/components/editPostForm.component.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+
+import { postUpdated } from '../slices/postsSlice';
+
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import TextField from '@material-ui/core/TextField';
+
+export default function EditPostForm(props) {
+    const [post, setPost] = useState(props.content);
+
+    const dispatch = useDispatch();
+
+    const handleClose = () => props.setOpen(false);
+
+    const handlePostChange = (event) => {
+        setPost(event.target.value);
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        dispatch(postUpdated({id: props.id, content: post}));
+        handleClose();
+    }
+
+    return (
+        <Dialog open={props.isOpen} onClose={handleClose}>
+            <DialogTitle>Edit Post</DialogTitle>
+            <DialogContent>
+                <form onSubmit={handleSubmit}>
+                    <TextField 
+                        fullWidth margin="normal" variant="outlined"
+                        placeholder="What are you celebrating?"
+                        multiline required type="text"
+                        onChange={handlePostChange} value={post}/>
+                    <Button
+                        color="primary" fullWidth variant="contained"
+                        type="submit">
+                        Save
+                    </Button>
+                </form>
+            </DialogContent>
+        </Dialog>
+    )
+}
