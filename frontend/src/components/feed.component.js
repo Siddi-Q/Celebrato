c8275bdd66ab9ad937363f1905374c0f13efe669
Saddique Shafi <saddiqueashafi@gmail.com>
2021-01-06 17:16:55 -0500

    Updated feed component to use selectAllPosts

--- frontend/src/components/feed.component.js
+++ frontend/src/components/feed.component.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 
+import { selectAllPosts } from '../slices/postsSlice';
+
 import Post from './post.component';
 
 export default function Feed() {
-    const posts = useSelector(state => state.posts);
+    const posts = useSelector(selectAllPosts);
 
     const renderedPosts = posts.map(post => (
         <Post key={post.id} id={post.id} content={post.content} userId={post.user} date={post.date}/>
