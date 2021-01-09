ce4558c591e2300ef1c65279f4615ab6c704f455
Saddique Shafi <saddiqueashafi@gmail.com>
2021-01-08 19:23:57 -0500

    Updated feed.component.js to use fetchPosts and new postsSlice state

--- frontend/src/components/feed.component.js
+++ frontend/src/components/feed.component.js
@@ -1,12 +1,21 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
-import { selectAllPosts } from '../slices/postsSlice';
+import { fetchPosts, selectAllPosts } from '../slices/postsSlice';
 
 import Post from './post.component';
 
 export default function Feed() {
+    const dispatch = useDispatch();
     const posts = useSelector(selectAllPosts);
+
+    const postStatus = useSelector(state => state.posts.status);
+
+    useEffect(() => {
+        if(postStatus === 'idle') {
+            dispatch(fetchPosts());
+        }
+    }, [dispatch, postStatus])
 
     const renderedPosts = posts.map(post => (
         <Post key={post.id} id={post.id} content={post.content} userId={post.user} date={post.date}/>
