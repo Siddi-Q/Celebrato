94267abbbe8310e43cb3943738e76bcf85bc1a07
Saddique Shafi <saddiqueashafi@gmail.com>
2021-01-12 18:05:05 -0500

    Updated store state to include authUser

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -2,10 +2,12 @@ import { configureStore } from '@reduxjs/toolkit';
 
 import postsReducer from '../slices/postsSlice';
 import usersReducer from '../slices/usersSlice';
+import authUserReducer from '../slices/authUserSlice';
 
 export default configureStore({
     reducer: {
         posts: postsReducer,
-        users: usersReducer
+        users: usersReducer,
+        authUser: authUserReducer
     }
 })
