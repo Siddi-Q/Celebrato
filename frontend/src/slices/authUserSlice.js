41c5b5f970a0b9acfed4b27909531c0278b43f07
Saddique Shafi <saddiqueashafi@gmail.com>
2021-01-14 14:22:16 -0500

    Added login async thunk

--- frontend/src/slices/authUserSlice.js
+++ frontend/src/slices/authUserSlice.js
@@ -1,15 +1,30 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
     user: {},
     isAuthenticated: false
 }
 
+export const login = createAsyncThunk('auth/login', async loginCred => {
+    const response = await fetch('/mockApi/login', {
+        method: 'POST',
+        body: JSON.stringify(loginCred)
+    });
+    const data = await response.json();
+    return data;
+});
+
 const authUserSlice = createSlice({
     name: 'authUser',
     initialState,
     reducers: {},
-    extraReducers: {}
+    extraReducers: {
+        [login.fulfilled]: (state, action) => {
+            console.log("fulfilled", action.payload);
+            state.isAuthenticated = true;
+            state.user = action.payload.user;
+        }
+    }
 });
 
 export default authUserSlice.reducer
