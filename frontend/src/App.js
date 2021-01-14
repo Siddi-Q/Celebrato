d6c35d0c4bca27cbd145c563c5b94fa5c4f0a798
Saddique Shafi <saddiqueashafi@gmail.com>
2021-01-14 14:27:39 -0500

    Updated App.js to use authUser

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 
-import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Link, Route, Switch, Redirect } from 'react-router-dom';
 
 import Login from './components/login.component';
 import Signup from './components/signup.component';
 import Home from './components/home.component';
+import { useSelector } from 'react-redux';
 
 function App() {
+    const isAuth = useSelector(state => state.authUser.isAuthenticated);
+
     return (
         <Router>
             <nav>
@@ -16,15 +19,9 @@ function App() {
             </nav>
 
             <Switch>
-                <Route exact path={["/", "/login"]}>
-                    <Login /> 
-                </Route>
-                <Route exact path="/signup">
-                    <Signup />
-                </Route>
-                <Route exact path="/home">
-                    <Home />
-                </Route>
+                <Route exact path={["/", "/login"]} render={() => isAuth ? <Redirect to="/home"/> : <Login /> } />
+                <Route exact path="/signup" render={() => isAuth ? <Redirect to="/home"/> : <Signup /> } />
+                <Route exact path="/home" render={() => isAuth ? <Home /> : <Login /> } />
             </Switch>
         </Router>
     );
