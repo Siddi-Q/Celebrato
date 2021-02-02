b6b7f9dc2f1f86144897aed4d94710a559a5037c
Saddique Shafi <saddiqueashafi@gmail.com>
2021-02-02 16:48:26 -0500

    Updated to use cors

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,13 @@
 require('dotenv').config();
 
 const express = require('express');
+var cors = require('cors');
 const router = require('./routers/route');
 
 const app = express();
 const port = 5000;
 
+app.use(cors());
 app.use(express.json());
 app.use(router);
 
