2de287d67439be21c0e81006fbe40f61242a4247
Saddique Shafi <saddiqueashafi@gmail.com>
2021-02-04 15:11:23 -0500

    Updated initialState; added/remove user to localStorage

--- frontend/src/slices/authUserSlice.js
+++ frontend/src/slices/authUserSlice.js
@@ -2,8 +2,8 @@ import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import { loginService, logoutService } from '../services/authService';
 
 const initialState = {
-    user: {},
-    isAuthenticated: false
+    user: JSON.parse(localStorage.getItem('user')),
+    isAuthenticated: Boolean(localStorage.getItem('token'))
 };
 
 export const login = createAsyncThunk('auth/login', async loginCred => {
@@ -25,11 +25,13 @@ const authUserSlice = createSlice({
             state.isAuthenticated = true;
             state.user = action.payload.user;
             localStorage.setItem('token', action.payload.authToken);
+            localStorage.setItem('user', JSON.stringify(action.payload.user));
         },
         [logout.fulfilled]: (state, action) => {
             state.isAuthenticated = false;
             state.user = {};
             localStorage.removeItem('token');
+            localStorage.removeItem('user');
         }
     }
 });
