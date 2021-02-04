6107d2f52200fa6275a23b82f7856b19f3f740b9
Saddique Shafi <saddiqueashafi@gmail.com>
2021-02-04 15:10:27 -0500

    Removed links

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { BrowserRouter as Router, Link, Route, Switch, Redirect } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 
 import Login from './components/login.component';
 import Signup from './components/signup.component';
@@ -12,12 +12,6 @@ function App() {
 
     return (
         <Router>
-            <nav>
-                <Link to="/login">Login</Link><br />
-                <Link to="/signup">Signup</Link><br />
-                <Link to="/home">Home</Link>
-            </nav>
-
             <Switch>
                 <Route exact path={["/", "/login"]} render={() => isAuth ? <Redirect to="/home"/> : <Login /> } />
                 <Route exact path="/signup" render={() => isAuth ? <Redirect to="/home"/> : <Signup /> } />
