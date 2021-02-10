ff958ef639a408d3c4000c056f4985a0cf4804ff
Saddique Shafi <saddiqueashafi@gmail.com>
2021-02-10 13:37:35 -0500

    Added signup thunk

--- frontend/src/slices/authUserSlice.js
+++ frontend/src/slices/authUserSlice.js
@@ -1,5 +1,5 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-import { loginService, logoutService } from '../services/authService';
+import { loginService, logoutService, signupService } from '../services/authService';
 
 const initialState = {
     user: JSON.parse(localStorage.getItem('user')),
@@ -14,6 +14,12 @@ export const login = createAsyncThunk('auth/login', async loginCred => {
 
 export const logout = createAsyncThunk('auth/logout', async () => {
     await logoutService();
+});
+
+export const signup =  createAsyncThunk('auth/signup', async newUserInfo => {
+    const response = await signupService(newUserInfo);
+    const data = response.data;
+    return data;
 });
 
 const authUserSlice = createSlice({
@@ -32,6 +38,12 @@ const authUserSlice = createSlice({
             state.user = {};
             localStorage.removeItem('token');
             localStorage.removeItem('user');
+        },
+        [signup.fulfilled]: (state, action) => {
+            state.isAuthenticated = true;
+            state.user = action.payload.user;
+            localStorage.setItem('token', action.payload.authToken);
+            localStorage.setItem('user', JSON.stringify(action.payload.user));
         }
     }
 });
