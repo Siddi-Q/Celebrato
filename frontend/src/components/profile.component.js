eb5492b83861b0a692fa1ec3fdbc4a1cca835bb8
Saddique Shafi <saddiqueashafi@gmail.com>
2021-02-11 13:06:51 -0500

    Created profile.component.js

---
+++ frontend/src/components/profile.component.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import Navbar from './navbar.component';
+
+export default function Profile() {
+    return (
+        <>
+            <Navbar />
+            <h1>Profile</h1>
+        </>
+    );
+}
