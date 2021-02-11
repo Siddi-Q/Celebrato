b4351d21a4cc3e4db22c6fd0eb10fbe2baae510c
Saddique Shafi <saddiqueashafi@gmail.com>
2021-02-11 15:55:11 -0500

    Updated profile component to use fetchAUsersPosts()

--- frontend/src/components/profile.component.js
+++ frontend/src/components/profile.component.js
@@ -1,12 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+import { useSelector } from 'react-redux';
+import { selectUser } from '../slices/authUserSlice';
+import { fetchAUsersPosts } from '../services/usersService'; 
 
 import Navbar from './navbar.component';
 
 export default function Profile() {
+    const [posts, setPosts] = useState([]);
+    const user = useSelector(selectUser);
+
+    useEffect(() => {
+        fetchAUsersPosts(user.user_id)
+        .then(res => {
+            setPosts(res.data.posts);
+        })
+        .catch(error => {
+            console.log("error:", error);
+        })
+    }, []);
+
     return (
         <>
             <Navbar />
             <h1>Profile</h1>
+            {posts.map(post => <p>{post.date} - {post.content}</p>)}
         </>
     );
 }
